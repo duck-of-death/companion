d70156e668959a019de0201cd838ec8946c807db
John McGuigan <john@upperbound.ca>
2019-12-09 22:43:41 -0700

    Weird python crashes...

--- dbtest.py
+++ dbtest.py
@@ -16,52 +16,52 @@ def open_database():
     
     # Directory to where ever COMP tables (ADT, ADM, ADI) are stored
     # Had a weird issue earlier where some tables had lowercaser extensions...
-    comp_directory = '/home/john/Comp4win'
+    comp_directory = '/home/john/Comp4win/'
 
     # Advantage ODBC driver fails is autocommit isn't set
     # DSN is set in /etc/odbc.ini
     conn = pyodbc.connect('DSN=Comp', autocommit=True)
     cur = conn.cursor()
 
-    tables = glob.glob(comp_directory + "/*.ADT")
+    tables = glob.glob(comp_directory + "*.ADT")
 
     for table in tables:
 
-        # This sucks but I can't seem to pass a param to execute?
-        cur.execute("SELECT COUNT(*) FROM [" + table + "]")
-        rowcount = cur.fetchone()[0]
-        cur.execute("SELECT TOP 1 *  FROM [" + table + "]")
+        try:
+            # This sucks but I can't seem to pass a param to execute?
+            cur.execute("SELECT TOP 1 *  FROM [" + table + "]")
 
-        # Tuple containing:
-            # column name (or alias, if specified in the SQL)
-            # type code
-            # display size (pyodbc does not set this value)
-            # internal size (in bytes)
-            # precision
-            # scale
-            # nullable (True/False)
+            # Tuple containing:
+                # column name (or alias, if specified in the SQL)
+                # type code
+                # display size (pyodbc does not set this value)
+                # internal size (in bytes)
+                # precision
+                # scale
+                # nullable (True/False)
 
-        table_desc = cur.description
-        print(table.replace(comp_directory + "/", "").replace(".ADT", ""), end=" ")
-        print(rowcount)
+            table_desc = cur.description
+            table_name = table.replace(comp_directory + "/", "").replace(".ADT", "")
 
-        for column in table_desc:
-            print("\t" + column[0], end=" ")
-            if column[1] == type(""):
-                print("string")
-            elif column[1] == type(False):
-                print("bool")
-            elif column[1] == type(0.0):
-                print("float")
-            elif column[1] == datetime.date:
-                print("datetime.date")
-            elif column[1] == type(0):
-                print("int")
-            elif column[1] == type(bytearray()):
-                print("bytearray")
+            for column in table_desc:
+                print(column[0] + " ", end="")
+                if column[1] == type(""):
+                    print("TEXT,", end="")
+                elif column[1] == type(False):
+                    print("BOOLEAN,", end="")
+                elif column[1] == type(0.0):
+                    print("FLOAT,", end="")
+                elif column[1] == datetime.date:
+                    print("DATE,", end="")
+                elif column[1] == type(0):
+                    print("INT,", end="")
+                elif column[1] == type(bytearray()):
+                    print("BLOB,", end="")
 
-            else:
-                pdb.set_trace()
+            print("")
+
+        except UnicodeDecodeError as e:
+            pdb.set_trace()
 
     return
 
