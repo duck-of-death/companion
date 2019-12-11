6623b96b5ec6fffa8bcfa5e17ae2d3b0ba4c9a21
John McGuigan <john@upperbound.ca>
2019-12-10 20:49:52 -0700

    Field name working

--- sea/tableview.c
+++ sea/tableview.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "ace.h"
 
 int main(int argc, char **argv)
 {
 	ADSHANDLE hCon;
 	ADSHANDLE hTable;
+	UNSIGNED8 *aucFieldName;
 	UNSIGNED16 usNumFields;
+	UNSIGNED16 usLength;
 	UNSIGNED32 ulNumRecords;
 	UNSIGNED32 ulRetCode;
 
@@ -23,6 +26,13 @@ int main(int argc, char **argv)
 	printf("ulRetCode = %d\n", ulRetCode);
 	printf("Number of records: %d\n", ulNumRecords);
 
+	// Fields start at 1
+	aucFieldName = malloc(sizeof(UNSIGNED8) * 128);
+	usLength = 128;
+	ulRetCode = AdsGetFieldName(hTable, 1, aucFieldName, &usLength); 
+	printf("ulRetCode = %d\n", ulRetCode);
+	printf("Field name: %s\n", aucFieldName);
+	free(aucFieldName);	
 
 
 	return 0;
